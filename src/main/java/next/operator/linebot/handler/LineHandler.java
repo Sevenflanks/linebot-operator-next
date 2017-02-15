933d7e8128a066bd36bf085aca9c001538979559
sevenflanks <godjdc@gmail.com>
2017-02-15 20:24:05 +0800

    no message

--- src/main/java/next/operator/linebot/handler/LineHandler.java
+++ src/main/java/next/operator/linebot/handler/LineHandler.java
@@ -1,14 +1,15 @@
 package next.operator.linebot.handler;
 
 import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.*;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Slf4j
 @LineMessageHandler
 public class LineHandler {
 
@@ -17,13 +18,43 @@ public class LineHandler {
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
+        log.info("TextMessageEvent: {}", event);
         return new TextMessage(event.getMessage().getText());
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
+        log.info("DefaultMessageEvent: {}", event);
+    }
+
+    @EventMapping
+    public void handleUnfollowEvent(UnfollowEvent event) {
+        log.info("unfollowed this bot: {}", event);
+    }
+
+    @EventMapping
+    public void handleFollowEvent(FollowEvent event) {
+        log.info("Followed this bot: {}", event);
+    }
+
+    @EventMapping
+    public void handleJoinEvent(JoinEvent event) {
+        log.info("Joined: {}", event);
+    }
+
+    @EventMapping
+    public void handlePostbackEvent(PostbackEvent event) {
+        log.info("Postbacked: {}", event);
+    }
+
+    @EventMapping
+    public void handleBeaconEvent(BeaconEvent event) {
+        log.info("Beacon: {}", event);
+    }
+
+    @EventMapping
+    public void handleOtherEvent(Event event) {
+        log.info("Received message(Ignored): {}", event);
     }
 
 }
