7942ae4608113b4d8a19bb85d735040346f7e0cf
Sevenflanks <godjdc@gmail.com>
2017-02-27 20:25:38 +0800

    no message

--- src/main/java/next/operator/linebot/executor/impl/ExrateExecuter.java
+++ src/main/java/next/operator/linebot/executor/impl/ExrateExecuter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ExrateExecuter implements FunctionExecutable {
@@ -20,13 +21,16 @@ public class ExrateExecuter implements FunctionExecutable {
   }
 
   final DecimalFormat decimalFormat = new DecimalFormat("#,###,###,##0.00000");
+  final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
   @Override
   public String execute(String... args) {
     if (args.length != 2) {
       return "打開的方式不對喔！要像這樣子: " + structure();
     } else {
       final CurrencyExrateModel exrate = currencyService.getExrate(args[0], args[1]);
-      return "1 " + exrate.getExFrom() + " = " + decimalFormat.format(exrate.getExrate()) + " " + exrate.getExTo();
+      return "匯率查詢：" +
+          "1 " + exrate.getExFrom() + " = " + decimalFormat.format(exrate.getExrate()) + " " + exrate.getExTo() +
+          ", 資料時間：" + dateTimeFormatter.format(exrate.getTime());
     }
   }
 
